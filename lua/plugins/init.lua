ae4eb103acb7bd0b324d76ebe0d83fe0cbe61b65
Israr Khan <israr.khan@gmail.com>
2024-11-04 13:04:57 -0800

    added Yazi.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -59,6 +59,32 @@ return {
       -- Configure vim-highlightedyank
       vim.g.highlightedyank_highlight_duration = 100  -- duration in milliseconds
     end,
+  },
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>-",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory" ,
+      }
+    },
+    ---@type YaziConfig
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = '<f1>',
+      },
+    },
   }
 
 
